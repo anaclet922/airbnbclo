2c8aa45b5e30723452c23bc45ddd3b7e53273831
Anaclet Ahishakiye <a.anaclet920@gmail.com>
2021-06-06 20:35:52 +0200

    s

--- src/component/Item.js
+++ src/component/Item.js
@@ -25,12 +25,41 @@ import {
   Switch
 } from 'react-router-dom';
 
-import { faBars, faUser, faStar, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faUser, faStar, faHeart, faRssSquare, faGift, faShareAlt } from '@fortawesome/free-solid-svg-icons'
 
 function Item (){
     return (
         <Router>
+            <Container style={{ marginTop: "30px" }}>
+                <Row>
+                    <Col md={12}>
+                        <a href="#" style={{ color: "#000" }}>All online experience</a>
+                        <br/>
+                        <br/>
+                        <FontAwesomeIcon icon={faRssSquare} /> ONLINE EXPERIENCE
+                        <br/>
+                        <span style={{ fontSize: "25px", fontWeight: 600, marginTop: "30px", marginBottom:"30px" }}>
+                            Living Room Legends Scavengers Hunt Game
+                        </span>
+                        <br/>
+                        <span>
+                            <FontAwesomeIcon icon={faStar} /> . Austin, United States . Part of Field Trips Online Experiences
+                        </span>
 
+                        <span className={"float-right "}>
+                            <span>
+                                <FontAwesomeIcon icon={faGift} /> Gift
+                            </span>
+                            <span>
+                                <FontAwesomeIcon icon={faShareAlt} /> Share
+                            </span>
+                            <span>
+                                <FontAwesomeIcon icon={faHeart} /> Save
+                            </span>
+                        </span>
+                    </Col>
+                </Row>
+            </Container>
         </Router>
     );
 }
